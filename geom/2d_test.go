bb622516d9029951a78835385d003bb301e97718
Ethan Burns <burns.ethan@gmail.com>
2013-06-05 11:19:11 -0400

    Add 2d geometry tests.

--- geom/2d_test.go
+++ geom/2d_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestImageCanvas_toImgCoords(t *testing.T) {
+func TestToImgCoords(t *testing.T) {
 	const (
 		w = 101
 		h = 4000
@@ -18,7 +18,6 @@ func TestImageCanvas_toImgCoords(t *testing.T) {
 		{w - 1, h - 1, w - 1, 0},
 		{0, h - 1, 0, 0},
 	}
-
 	c := ImageCanvas{image.NewRGBA(image.Rect(0, 0, w, h))}
 	for _, test := range tests {
 		x, y := c.toImgCoords(test.x0, test.y0)
@@ -27,5 +26,25 @@ func TestImageCanvas_toImgCoords(t *testing.T) {
 		}
 		t.Errorf("Expect %d,%d to transform to %d,%d, but got %d,%d\n",
 			test.x0, test.y0, test.x1, test.y1, x, y)
+	}
+}
+
+func TestSegmentNormal(t *testing.T) {
+	tests := []struct {
+		s0, s1 Point
+		n      Vector
+	}{
+		{Point{-1, 0}, Point{1, 0}, Vector{0, 1}},
+		{Point{1, 0}, Point{-1, 0}, Vector{0, -1}},
+		{Point{0, -1}, Point{0, 1}, Vector{-1, 0}},
+		{Point{0, 1}, Point{0, -1}, Vector{1, 0}},
+	}
+
+	for _, test := range tests {
+		n := Segment{test.s0, test.s1}.Normal()
+		if n.Equals(test.n) {
+			continue
+		}
+		t.Errorf("Expect normal of %v to %v to be %v, got %v", test.s0, test.s1, test.n, n)
 	}
 }
